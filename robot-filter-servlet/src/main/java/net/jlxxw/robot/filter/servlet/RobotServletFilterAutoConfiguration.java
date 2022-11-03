dd4cef72ca93aebacef2018d9e301ff537047729
izhanxiumei <lcy19930619@163.com>
2022-11-03 21:32:19 +0800

    fix todo

--- robot-filter-servlet/src/main/java/net/jlxxw/robot/filter/servlet/RobotServletFilterAutoConfiguration.java
+++ robot-filter-servlet/src/main/java/net/jlxxw/robot/filter/servlet/RobotServletFilterAutoConfiguration.java
@@ -1,6 +1,7 @@
 package net.jlxxw.robot.filter.servlet;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.servlet.Filter;
@@ -9,6 +10,7 @@ import net.jlxxw.robot.filter.common.log.LogUtils;
 import net.jlxxw.robot.filter.config.properties.FilterProperties;
 import net.jlxxw.robot.filter.config.properties.RobotFilterProperties;
 import net.jlxxw.robot.filter.servlet.template.AbstractFilterTemplate;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanCreationException;
@@ -47,14 +49,31 @@ public class RobotServletFilterAutoConfiguration implements ApplicationRunner {
         List<FilterProperties> filters = robotFilterProperties.getFilters();
         filters.sort(Comparator.comparing(FilterProperties::getOrder));
 
+        Set<String> nameSet = new HashSet<>();
+        Set<String> classNameSet = new HashSet<>();
         if (beanFactory instanceof DefaultListableBeanFactory){
             DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory)beanFactory;
 
             for (FilterProperties filterProperties : filters) {
                 String name = filterProperties.getName();
+                if (StringUtils.isBlank(name)){
+                    throw new BeanCreationException("filter name is not null !!!");
+                }
+                if (nameSet.contains(name)){
+                    throw new BeanCreationException("filter name repeat :" + name);
+                }
+                nameSet.add(name);
+
                 String className = filterProperties.getClassName();
+                if (StringUtils.isBlank(className)){
+                    throw new BeanCreationException("className name is not null !!!");
+                }
+                if (nameSet.contains(className)){
+                    throw new BeanCreationException("className name repeat :" + name);
+                }
+                classNameSet.add(className);
+
                 Set<String> urlPattern = filterProperties.getUrlPattern();
-                // todo param check to lcy 2022/11/05 done
 
                 Class<?> clazz = Class.forName(className);
                 if (!AbstractFilterTemplate.class.isAssignableFrom(clazz)){
