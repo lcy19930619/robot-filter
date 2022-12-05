7c958112a5622dd0fae8576c99c08c60daee9068
冷春阳 <lcy19930619@163.com>
2022-12-05 19:29:40 +0800

    add temp black list

--- robot-filter-data-share/src/main/java/net/jlxxw/robot/filter/data/share/netty/listener/ApplicationEventNettyListener.java
+++ robot-filter-data-share/src/main/java/net/jlxxw/robot/filter/data/share/netty/listener/ApplicationEventNettyListener.java
@@ -1,6 +1,7 @@
 package net.jlxxw.robot.filter.data.share.netty.listener;
 
 import net.jlxxw.robot.filter.common.event.AddClientIdToBlackListEvent;
+import net.jlxxw.robot.filter.common.event.AddIpToBlackListEvent;
 import net.jlxxw.robot.filter.common.event.NettySendDataEvent;
 import net.jlxxw.robot.filter.common.event.ReceiveRequestEvent;
 import net.jlxxw.robot.filter.data.share.netty.protocol.protobuf.RequestProtocol;
@@ -23,13 +24,31 @@ public class ApplicationEventNettyListener {
         applicationContext.publishEvent(new NettySendDataEvent(convert));
     }
 
+    @EventListener(AddClientIdToBlackListEvent.class)
     public void addTempBlackList(AddClientIdToBlackListEvent event){
+        RequestProtocol convert = convert(event);
+        applicationContext.publishEvent(new NettySendDataEvent(convert));
+    }
 
+    @EventListener(AddIpToBlackListEvent.class)
+    public void addTempBlackList(AddIpToBlackListEvent event){
+        RequestProtocol convert = convert(event);
+        applicationContext.publishEvent(new NettySendDataEvent(convert));
     }
 
 
 
     public RequestProtocol convert(ReceiveRequestEvent event){
+        // todo convert
+        return  RequestProtocol.newBuilder().build();
+    }
+
+    public RequestProtocol convert(AddClientIdToBlackListEvent event){
+        // todo convert
+        return  RequestProtocol.newBuilder().build();
+    }
+
+    public RequestProtocol convert(AddIpToBlackListEvent event){
         // todo convert
         return  RequestProtocol.newBuilder().build();
     }
